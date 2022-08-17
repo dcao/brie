0ce5f4eed4d3654951beaceedd526918252697a5
David Cao <david@cao.sh>
2022-08-17 12:51:03 -0700

    bucket calc change

--- brie/src/simple_hash.rs
+++ brie/src/simple_hash.rs
@@ -7,6 +7,25 @@ use crate::{sorted::vec::BumpVec, Oneshot};
 
 use std::{hash::{Hash, Hasher}, mem::MaybeUninit};
 
+// #[derive(Debug)]
+// pub enum Trie<'b, E, const N: usize> {
+//     Empty,
+//     Data(Box<'b, Data<'b, [E; N]>>),
+//     Map(BumpVec<'b, Entry<'b, E, N>>),
+// }
+
+// impl<'b, E, const N: usize> Default for Trie<'b, E, N> {
+//     fn default() -> Self {
+//         Trie::Empty
+//     }
+// }
+
+// #[derive(Debug, Default)]
+// pub struct Entry<'b, E, const N: usize> {
+//     hash: usize,
+//     ptr: Trie<'b, E, N>,
+// }
+
 pub enum Trie<'b, E, const N: usize> {
     Empty,
     Data(Box<'b, Data<'b, [E; N]>>),
@@ -27,23 +46,24 @@ where
             Trie::Empty => None,
             Trie::Data(_) => None,
             Trie::Map(vs) => {
-                let l = vs.len();
+                let l = vs.len().log2(); // TODO: perf
                 Some(&mut vs[Self::get_bucket::<H>(key, l)])
             }
         }
     }
 
-    fn init(&mut self, buckets: usize, bump: &'b Bump) {
+    fn init(&mut self, bits: u32, bump: &'b Bump) {
         match self {
             Trie::Empty => {
-                *self = Trie::map(buckets, bump);
+                *self = Trie::map(bits, bump);
             }
             Trie::Data(_) => {}
             Trie::Map(_) => {}
         }
     }
 
-    fn map(buckets: usize, bump: &'b Bump) -> Self {
+    fn map(bits: u32, bump: &'b Bump) -> Self {
+        let buckets = 2_usize.pow(bits);
         let mut bv = BumpVec::with_capacity_in(buckets, bump);
         for _i in 0..buckets {
             bv.push(Self::Empty, bump);
@@ -52,15 +72,15 @@ where
         Self::Map(bv)
     }
 
-    fn calc_buckets(iter_len: usize) -> usize {
+    fn calc_bits(iter_len: usize) -> u32 {
         let sz = iter_len as f64;
-        ((sz * 1.25) as usize).next_power_of_two()
+        (sz * 1.25).log2().ceil() as u32
     }
 
-    fn get_bucket<H: Hasher + Default>(elem: &E, buckets: usize) -> usize {
+    fn get_bucket<H: Hasher + Default>(elem: &E, bits: u32) -> usize {
         let mut h = H::default();
         elem.hash(&mut h);
-        (h.finish() % buckets as u64).try_into().unwrap()
+        (h.finish() >> (usize::BITS - bits as u32) as u64).try_into().unwrap()
     }
 }
 
@@ -111,7 +131,7 @@ where
 
             let mut it = tup.iter().zip(cardinalities.iter());
             while let Some((v, buckets)) = it.next() {
-                cur.init(Self::calc_buckets(*buckets), bump);
+                cur.init(Self::calc_bits(*buckets), bump);
                 cur = cur.get::<ahash::AHasher>(v).unwrap();
             }
 
